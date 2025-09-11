028ada679fac845b9c53de5aeeb90a8ac84a9837
alilirayane <alili.rayan06@gmail.com>
2025-09-11 10:37:09 +0200

    correction bugs

--- cageots.c
+++ cageots.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define N 4  
+#define N 10 
 #define M 4
 
-void solve_cageots_de_fraise(int b[M][N])
+void solve_cageots_de_fraise(int b[M][N+1])
 {
     int choixOpt[M][N + 1]; // Tableau des choix optimaux
     double Benef[N + 1];    // Benefice optimal ( en place , par ligne )
@@ -13,11 +13,18 @@ void solve_cageots_de_fraise(int b[M][N])
         Benef[p] = b[0][p];
         choixOpt[0][p] = p;
     }
+    printf("benef \n");
+    for (int p = 0; p <= N; ++p)
+    { 
+        printf("%f ",Benef[p]);
+        
+    }
+    printf("\n");
     for (int m = 1; m < M; ++m)
     {
         for (int p = N; p >= 0; --p)
         {
-            int max = b[m][p];
+            double max = b[m][p];
             int argmax = p;
             for (int k = 0; k <= p - 1; ++k)
             {
@@ -43,6 +50,13 @@ void solve_cageots_de_fraise(int b[M][N])
 
     // Affichage du résultat
     printf("Bénéfice optimal = %.2f\n", Benef[N]);
+    printf("benef \n");
+    for (int p = 0; p <= N; ++p)
+    { 
+        printf("%f ",Benef[p]);
+        
+    }
+    printf("\n");
     printf("Répartition des cageots :\n");
     for (int m = 0; m < M; ++m) {
         printf(" Magasin %d : %d cageots\n", m, repartition[m]);
@@ -50,20 +64,24 @@ void solve_cageots_de_fraise(int b[M][N])
     
 
 }
-void remplir_matrice(int b[M][N]) {
+void remplir_matrice(int b[M][N+1]) {
     // Initialiser le générateur de nombres aléatoires
     srand(time(NULL));
 
     // Remplissage de la matrice avec des valeurs positives
     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            b[i][j] = rand() % 15 + 1; // valeurs entre 1 et 100
+        for (int j = 0; j <= N; j++) {
+            if (j == 0) {
+                b[i][j] = 0;           // première colonne forcée à 0
+            } else {
+                b[i][j] = i + j;
+            }
         }
     }
 }
-void afficher_matrice(int b[M][N]) {
+void afficher_matrice(int b[M][N+1]) {
     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j <= N; j++) {
             printf("%3d ", b[i][j]);
         }
         printf("\n");
@@ -71,7 +89,7 @@ void afficher_matrice(int b[M][N]) {
 }
 int main(int argc, char *argvar[])
 {
-    int b[M][N];
+    int b[M][N+1];
     remplir_matrice(b);
     afficher_matrice(b);
     solve_cageots_de_fraise(b);
